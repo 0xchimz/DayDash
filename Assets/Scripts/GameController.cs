4ac4c1355636a4bd116feb71dad4928a74476e77
ChinCluBi <chincub@gmail.com>
2016-04-21 16:28:12 +0700

    Add socket.io and create current player

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public Player playerGameObj;
+
+	// Use this for initialization
+	void Start () {
+		GameObject player = GameObject.Instantiate (playerGameObj.gameObject, playerGameObj.position, Quaternion.identity) as GameObject;
+		Player playerCom = player.GetComponent<Player> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
