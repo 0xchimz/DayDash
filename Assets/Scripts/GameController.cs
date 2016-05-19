98f82ea2b773c62c3624dbb052b668400a2cdb9a
ChinCluBi <chincub@gmail.com>
2016-05-19 13:54:58 +0700

    Connect to server

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -11,12 +11,31 @@ public class GameController : MonoBehaviour {
 	Player currentPlayer;
 
 	void Start () {
+		socketIO.On ("USER_CONNECTED", OnUserConnected);
+		socketIO.On("USER_DISCONNECTED", OnUserDisconnected );
+
 		GameObject player = GameObject.Instantiate (playerGameObj.gameObject, playerGameObj.position, Quaternion.identity) as GameObject;
 		currentPlayer = player.GetComponent<Player> ();
+
+		StartCoroutine( "CalltoServer" );
 	}
 
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	private IEnumerator CalltoServer(){
+		yield return new WaitForSeconds(1f);
+		Debug.Log("Send join room message to the server");
+		socketIO.Emit("JOIN_ROOM");
+	}
+
+	void OnUserDisconnected (SocketIOEvent obj) {
+		Debug.Log ("Desconnect from server.");
+	}
+
+	void OnUserConnected (SocketIOEvent obj) {
+		Debug.Log ("Joining Room.");
 	}
 }
