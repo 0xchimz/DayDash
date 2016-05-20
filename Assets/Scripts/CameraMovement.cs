65d6b37855f2bd238ed93fea7cbf381c1f3eb283
Momomomomo <topolite@hotmail.com>
2016-05-20 23:01:25 +0700

    camera can follow character

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+
+	public GameObject player;
+	private Vector3 camPosition;
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		camPosition = new Vector3 (player.transform.position.x -0.5f, player.transform.position.y + 6, player.transform.position.z - 6);
+		transform.position = camPosition;
+
+	}
+		
+
+}
