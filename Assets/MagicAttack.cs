581173536aa68ad4024c6e3121b4749b125ddd1e
Momomomomo <topolite@hotmail.com>
2016-05-27 01:40:48 +0700

    magic can trigger the object

---
+++ Assets/MagicAttack.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MagicAttack : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter (Collider gameElement) {
+		Debug.Log (".... Cause someone damage");
+	
+	}
+}
